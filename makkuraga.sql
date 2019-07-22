208fc9a00e74c6cb9451200d6d851221164feed6
Al Riefqy Dasmito <ekydesmito@gmail.com>
2019-07-22 09:46:13 -0700

    db udate

--- makkuraga.sql
+++ makkuraga.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2019 at 06:34 PM
+-- Generation Time: Jul 22, 2019 at 06:45 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -42,16 +42,49 @@ CREATE TABLE `backlog` (
   `description` text NOT NULL,
   `reminder_km` bigint(100) NOT NULL,
   `reminder_hours_meter` bigint(100) NOT NULL,
-  `location` varchar(100) NOT NULL
+  `location` varchar(100) NOT NULL,
+  `mechanic` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `backlog`
 --
 
-INSERT INTO `backlog` (`id_backlog`, `down_date`, `up_date`, `ID`, `model`, `hours_meter`, `indication`, `priority`, `status`, `history`, `description`, `reminder_km`, `reminder_hours_meter`, `location`) VALUES
-(655, '2019-07-04', '2019-07-09', 'MAN', '8GH', 1, 'Indication', 'P2', 'MN', 'done', '', 190, 190, 'a'),
-(656, '0000-00-00', '0000-00-00', 'MAN', '8GH', 1, 'Indication', '', 'MN', 'backlog', '', 21212, 121212, 'a');
+INSERT INTO `backlog` (`id_backlog`, `down_date`, `up_date`, `ID`, `model`, `hours_meter`, `indication`, `priority`, `status`, `history`, `description`, `reminder_km`, `reminder_hours_meter`, `location`, `mechanic`) VALUES
+(655, '2019-07-04', '2019-07-09', 'MAN', '8GH', 1, 'Indication', 'P2', 'MN', 'done', '', 190, 190, 'a', ''),
+(656, '2019-07-03', '0000-00-00', 'MAN', '8GH', 1, 'Indication', '', 'MN', 'onprogress', '1', 21212, 121212, 'a', 'Spiderman'),
+(657, '0000-00-00', '0000-00-00', 'MAN', '8GH', 1, 'mn', 'P2', 'Breakdown', 'backlog', '', 100, 1000, 'a', ''),
+(658, '0000-00-00', '0000-00-00', 'MAN', '8GH', 1, 'Indication', 'P1', 'Waiting Part', 'backlog', '', 100, 1000, 'a', ''),
+(659, '0000-00-00', '0000-00-00', '1627', 'MAN', 1, 'Indication', 'P1', 'Waiting Part', 'backlog', '', 1, 9, 'a', ''),
+(660, '0000-00-00', '0000-00-00', '1627', 'MAN', 1, 'MN', 'P1', 'Waiting Part', 'backlog', '', 10, 10, 'a', ''),
+(661, '0000-00-00', '0000-00-00', '9012', '8GH', 1, 'Indication', 'P1', 'Waiting Part', 'backlog', '', 100, 100, 'a', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mechanic`
+--
+
+CREATE TABLE `mechanic` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `location` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mechanic`
+--
+
+INSERT INTO `mechanic` (`id`, `name`, `location`) VALUES
+(1, 'Mechanic A', 'Asera'),
+(2, 'Mechanic B', 'Kodal'),
+(3, 'mn', 'Asera'),
+(4, 'makkuraga', 'Kodal'),
+(5, 'MAN', 'Makassar'),
+(6, 'Spiderman', 'Kodal'),
+(7, 'Iron Man', 'Kodal'),
+(8, 'Hulk', 'Kodal'),
+(9, 'Thor', 'Kodal');
 
 -- --------------------------------------------------------
 
@@ -88,6 +121,12 @@ ALTER TABLE `backlog`
   ADD PRIMARY KEY (`id_backlog`);
 
 --
+-- Indexes for table `mechanic`
+--
+ALTER TABLE `mechanic`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -101,7 +140,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `backlog`
 --
 ALTER TABLE `backlog`
-  MODIFY `id_backlog` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=657;
+  MODIFY `id_backlog` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=662;
+
+--
+-- AUTO_INCREMENT for table `mechanic`
+--
+ALTER TABLE `mechanic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user`
